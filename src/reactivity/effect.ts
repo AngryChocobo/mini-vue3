08d57f95349e26c9d7c79e1af6b6d1ee7d14b8b9
AngryChocobo <1347237169@qq.com>
2022-05-28 17:35:27 +0800

    🎨 trackDep & triggerDep

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -52,6 +52,10 @@ export function trigger(target: any, key: string | symbol) {
   if (!depsMap) return;
   const deps = depsMap.get(key);
   if (!deps) return;
+  triggerDep(deps);
+}
+
+export function triggerDep(deps: Set<any>) {
   deps.forEach((dep) => {
     if (dep.options?.scheduler) {
       dep.options.scheduler();
@@ -77,6 +81,10 @@ export function track(target: any, key: string | symbol) {
   if (!dep) {
     depsMap.set(key, (dep = new Set()));
   }
+  trackDep(dep);
+}
+
+export function trackDep(dep: Set<any>) {
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
 }
